a45c36b8f88b4bbc13204f78d348a9d0b47802f8
Peter Louis Anderson <142972091+peter-louisx@users.noreply.github.com>
2025-04-29 12:02:47 +0700

    #fix route seller and buyer

--- src/App.tsx
+++ src/App.tsx
@@ -3,47 +3,54 @@ import Profile from "./pages/Profile";
 import Product from "./pages/Product";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Landing from "./pages/Landing";
 import ProductListing from "./pages/Products";
 import BrandDetail from "./pages/BrandDetail";
 import ScrollToTop from "./components/ScrollToTop";
 import GreenGame from "./components/GreenGame";
+import BuyerLayout from "./components/BuyerLayout";
 
 // seller's
-import Layout from './components/seller/Layout';
-import SDashboard from './pages/seller/Dashboard';
-import SProducts from './pages/seller/Products';
-import SOrders from './pages/seller/Orders';
-import SOrderDetail from './pages/seller/OrderDetail';
-import SAccount from './pages/seller/Account';
-import SWithdrawal from './pages/seller/Withdrawal'
-import { AppProvider } from './context/AppContext';
+import SellerLayout from "./components/seller/Layout";
+import SDashboard from "./pages/seller/Dashboard";
+import SProducts from "./pages/seller/Products";
+import SOrders from "./pages/seller/Orders";
+import SOrderDetail from "./pages/seller/OrderDetail";
+import SAccount from "./pages/seller/Account";
+import SWithdrawal from "./pages/seller/Withdrawal";
+import { AppProvider } from "./context/AppContext";
 
 // Blog
 import Blog from "./pages/Blog";
-import BlogDetail from './pages/BlogDetail';
+import BlogDetail from "./pages/BlogDetail";
 
 // buyer
 function App() {
   return (
     <Router>
-      <Header />
       <ScrollToTop />
       <Routes>
-        <Route path="/" element={<Landing />} />
+        <Route element={<BuyerLayout />}>
+          <Route path="/" element={<Landing />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/products" element={<ProductListing />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/brand/:id" element={<BrandDetail />} />
+          <Route path="/green-game" element={<GreenGame />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:id" element={<BlogDetail />} />
+        </Route>
 
-        {/* Protected Routes */}
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/products" element={<ProductListing />} />
-        <Route path="/product/:id" element={<Product />} />
-        <Route path="/brand/:id" element={<BrandDetail />} />
-        <Route path="/green-game" element={<GreenGame />} />
-        
+        <Route path="/seller" element={<SellerLayout />}>
+          <Route path="/seller" element={<SDashboard />} />
+          <Route path="/seller/products" element={<SProducts />} />
+          <Route path="/seller/orders" element={<SOrders />} />
+          <Route path="/seller/orders/:id" element={<SOrderDetail />} />
+          <Route path="/seller/account" element={<SAccount />} />
+          <Route path="/seller/withdrawal" element={<SWithdrawal />} />
+        </Route>
       </Routes>
-      <Footer />
     </Router>
   );
 }
@@ -54,32 +61,17 @@ function App() {
 //     <AppProvider>
 //       <Router>
 //         <Routes>
-//           <Route path="/" element={<Layout />}>
-//             <Route index element={<SDashboard />} />
-//             <Route path="products" element={<SProducts />} />
-//             <Route path="orders" element={<SOrders />} />
-//             <Route path="orders/:id" element={<SOrderDetail />} />
-//             <Route path="account" element={<SAccount />} />
-//             <Route path="withdrawal" element={<SWithdrawal />} />
+//           <Route path="/seller" element={<SellerLayout />}>
+//             <Route path="/seller" element={<SDashboard />} />
+//             <Route path="/seller/products" element={<SProducts />} />
+//             <Route path="/seller/orders" element={<SOrders />} />
+//             <Route path="/seller/orders/:id" element={<SOrderDetail />} />
+//             <Route path="/seller/account" element={<SAccount />} />
+//             <Route path="/seller/withdrawal" element={<SWithdrawal />} />
 //           </Route>
 //         </Routes>
 //       </Router>
 //     </AppProvider>
-//   );
-// }
-
-// Blog
-// function App() {
-//   return (
-//     <Router>
-//       <Header />
-//       <Routes>
-//       <Route path="/" element={<Landing />} />
-//         <Route path="/blog" element={<Blog />} />
-//         <Route path="/blog/:id" element={<BlogDetail />} />
-//       </Routes>
-//       <Footer />
-//     </Router>
 //   );
 // }
 
--- src/components/seller/Layout.tsx
+++ src/components/seller/Layout.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Sidebar from '../../components/seller/Sidebar';
-import Header from '../../components/seller/Header';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "../../components/seller/Sidebar";
+import { AppProvider } from "../../context/AppContext";
+import Header from "../../components/seller/Header";
 
-const Layout: React.FC = () => {
+const SellerLayout: React.FC = () => {
   return (
-    <div className="flex h-screen bg-gray-50">
-      <Sidebar />
-      <div className="flex flex-col flex-1 overflow-hidden">
-        <Header />
-        <main className="flex-1 overflow-y-auto p-4 md:p-6 bg-gray-50">
-          <Outlet />
-        </main>
+    <AppProvider>
+      <div className="flex h-screen bg-gray-50">
+        <Sidebar />
+        <div className="flex flex-col flex-1 overflow-hidden">
+          <Header />
+          <main className="flex-1 overflow-y-auto p-4 md:p-6 bg-gray-50">
+            <Outlet />
+          </main>
+        </div>
       </div>
-    </div>
+    </AppProvider>
   );
 };
 
-export default Layout;
+export default SellerLayout;
